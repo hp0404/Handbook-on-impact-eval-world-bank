7f9948869c79678cd1edb1379d860a1839fefe8b
hp0404 <hryhorii.pavlenko@gmail.com>
2020-10-28 23:12:43 +0200

    replication code for random.R; up to t-test

---
+++ Python/random.py
@@ -0,0 +1,37 @@
+import numpy as np 
+import pandas as pd
+from scipy import stats
+
+
+def run_ttest(table, values, by, two_sample=True):
+    t, pvalue = stats.ttest_ind(
+        table.loc[table[by].eq(1), values],
+        table.loc[table[by].eq(0), values],
+        equal_var=two_sample
+    )
+    test_type = "Two Sample t-test" if two_sample else "Welch Two Sample t-test"
+    print(f"{test_type}; data: {values} by {by}; {t=}, {pvalue=}")
+
+
+data = pd.read_csv("./../data/hh_98.csv").rename(columns={"Unnamed: 0": "X"})
+
+###########
+# Subset
+###########
+
+df = data.assign(
+    lexptot=np.log(1 + data["exptot"]), 
+    lnland=np.log(1 + data["hhland"] / 100), 
+    vill=data["thanaid"] * 10 + data["villid"]
+)
+df["progvillm"] = df.groupby("vill")["dmmfd"].transform("max")
+df["progvillf"] = df.groupby("vill")["dfmfd"].transform("max")
+
+################################
+# Impacts of program placement
+################################
+
+# t-test
+
+run_ttest(df, "lexptot", "progvillf")
+run_ttest(df, "lexptot", "progvillm")
