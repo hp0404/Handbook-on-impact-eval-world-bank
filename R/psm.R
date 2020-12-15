a0ac7c92f84106a1679bf22e6f5e23af711b7a20
hp0404 <hryhorii.pavlenko@gmail.com>
2020-12-15 17:54:34 +0200

    Боксплоти

--- R/psm.R
+++ R/psm.R
@@ -1,10 +1,24 @@
+library(tidyr)
 library(dplyr)
 library(readxl)
 library(MatchIt)
-
+library(ggplot2)
 
 setwd("C:/github/Rcode-handbook-on-impact-eval-world-bank/")
 data <- read_excel("data/dataset.xls")
+
+# boxplots
+data %>%
+  gather(period, value, -treatment, -category, -product) %>%
+  mutate(
+    period = factor(period, levels = c("before", "after")),
+    treatment = ifelse(treatment == 1, "treatment", "control") 
+    ) %>%
+  ggplot(aes(x = period, y = value, group = period)) +
+  geom_boxplot() +
+  facet_wrap(~ treatment) +
+  xlab("Період (до та після втручання)") + ylab("Індекс споживчих цін") +
+  theme_bw() 
 
 
 # matching
@@ -15,9 +29,17 @@ summary(m.out)
 balanced <- match.data(m.out)
 balanced
 
+# Matched control group 
+balanced %>% filter(treatment == 0)
+
+
+# Unmatched rows
+left_join(data, balanced, by = "product") %>% filter(is.na(category.y))
+
+
 # DiD
 balanced %>%
   mutate(dif = after - before) %>%
   group_by(treatment) %>%
-  summarise(mean_effect = mean(dif)) %>%
-  mutate(total_effect = mean_effect - lag(mean_effect, default = first(mean_effect)))
+  summarise(mean_group_effect = mean(dif)) %>%
+  mutate(mean_total_effect = mean_group_effect - lag(mean_group_effect, default = first(mean_group_effect)))
